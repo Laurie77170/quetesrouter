3ebabdadd37c1d5d9cd84c15a3c4e3fdddec36c3
Laurie77170 <[Laurie77170](mailto:lauriebou1984@gmail.com)>
2023-01-23 16:59:59 +0100

    quete

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+const ROUTES: Routes = [
+  { path: '', component: UserProfileComponent},
+  { path: 'user-profile', component: UserProfileComponent },
+  { path: 'sign-up', component: SignUpComponent },
+];
+
+export { ROUTES };
